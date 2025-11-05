d7d930ae66ea04d63ef5a4474ca25648044b0a15
mvoro <mvorontsov713@gmail.com>
2025-11-05 17:13:01 +0100

    commit init

---
+++ src/app/Main.java
@@ -0,0 +1,47 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String email = askEmail();
+            System.out.println("Email принят: " + email);
+        } catch (NotCorrectEmailFormatException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+
+    public static String askEmail() throws NotCorrectEmailFormatException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите email: ");
+        String email = scanner.nextLine();
+
+        List<String> errors = new ArrayList<>();
+
+        if (email.length() < 5)
+            //throw new NotCorrectEmailFormatException("Email должен содержать не менее 5 символов");
+            errors.add("строка короче 5 символов");
+
+        if (!email.contains("@"))
+            //throw new NotCorrectEmailFormatException("Email должен содержать символ '@'");
+            errors.add("отсутствует символ '@'");
+
+        /*if (email.startsWith("@") || email.endsWith("@"))
+            throw new NotCorrectEmailFormatException("Символ '@' не должен быть первым или последним");
+         */
+        if (email.startsWith("@"))
+            errors.add("'@' не должна быть первой");
+        if (email.endsWith("@"))
+            errors.add("'@' не должна быть последней");
+
+        if (!errors.isEmpty()) {
+            throw new NotCorrectEmailFormatException(String.join(", ", errors));
+
+        }
+        return email;
+    }
+
+}
---
+++ src/app/NotCorrectEmailFormatException.java
@@ -0,0 +1,7 @@
+package app;
+
+public class NotCorrectEmailFormatException extends RuntimeException {
+    public NotCorrectEmailFormatException(String message) {
+        super(message);
+    }
+}
